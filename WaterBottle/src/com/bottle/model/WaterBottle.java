6c9c84b7cb0183f0e2e3ccd99ff90f7eafdd5615
NarendraCoforge <narendra.coforge@gmail.com>
2022-11-23 09:55:12 +0530

    narendra- as per requeste those have been added

--- WaterBottle/src/com/bottle/model/WaterBottle.java
+++ WaterBottle/src/com/bottle/model/WaterBottle.java
@@ -5,7 +5,38 @@ public class WaterBottle {
 	private int id;
 	private String name;
 	private double price;
+	public WaterBottle() {
+		super();
+	}
+	public WaterBottle(int id, String name, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "WaterBottle [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
 	
-	// write setter and getter,as well tostring after delte comment
+	
 	
 }

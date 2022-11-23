0ada1aa6ccdc9135f7e33ef910039adbf577da71
NarendraCoforge <narendra.coforge@gmail.com>
2022-11-23 09:48:39 +0530

    step1 move to git host

---
+++ WaterBottle/src/com/bottle/model/WaterBottle.java
@@ -0,0 +1,10 @@
+package com.bottle.model;
+
+public class WaterBottle {
+
+	private int id;
+	private String name;
+	private double price;
+	
+	
+}
---
+++ WaterBottle/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author BNR
+ *
+ */
+module WaterBottle {
+}
